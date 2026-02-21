c5d088a9bc39ba0a2629b590dfc70ff2ea684ed6
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-02-21 12:30:08 +0200

    add

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from urllib import request
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
-import openai
+from openai import OpenAI
 import os
 import requests
 import base64
@@ -36,12 +38,51 @@ class ImageResponse(BaseModel):
     model: str
     size: str
 
+print(os.environ.get("OPENAI_API_KEY"))
 # config open ai
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
+# openai.api_key = os.environ.get("OPENAI_API_KEY")
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to OpenAI API!"}
+
+
+@app.post("/api/openai/chat", response_model=ChatResponse)
+async def chat_completion(request: ChatRequest):
+    try:
+        response = client.chat.completions.create(
+            model=request.model,
+            messages=[
+                # {
+                #     "role": "developer",
+                #     "content": "Say this is a test",
+                # },
+                {
+                    "role": "user",
+                    "content": request.message,
+                }
+            ],
+        )
+
+        answer = response.choices[0].message.content
+        # token_used = response.token_used
+        # token_used = response.user.token_used
+        return ChatResponse(
+            message=answer,
+            model=request.model,
+            token_used=0
+        )
+    # except client. as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
